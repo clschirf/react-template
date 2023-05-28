c510cd0873ffbe27f45dc3268b63ab21ef97dbf7
clschirf <clschirf@gmail.com>
2023-05-27 21:29:48 -0500

    jest works now too

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,17 @@
-export default {
-  displayName: 'react-template',
-  // preset: './jest.preset.js',
-  transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
-  },
+import type { Config } from 'jest';
+
+const config: Config = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
     '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
   ],
-  setupFiles: ['./jestSetup.js'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  setupFiles: ['./jest.setup.ts'],
+  clearMocks: true,
+  errorOnDeprecated: true,
 };
+
+export default config;
---
+++ jest.setup.ts
@@ -0,0 +1,1 @@
+jest.useFakeTimers().setSystemTime(new Date('2023-02-01'));
